d62c3b0d01569f0b99f6e01b74620cb495565b94
lucasSouzaWebDev <lucasdesouzadubena@gmail.com>
2021-11-09 20:30:46 -0300

    Correção questão 2 e feita questão 3

--- app/Http/Controllers/Tarefa1Controller.php
+++ app/Http/Controllers/Tarefa1Controller.php
@@ -40,19 +40,16 @@ class Tarefa1Controller extends Controller
             'Novembro',
             'Dezembro',
         );
-        return isset($arrayMes[$mes]) ? $arrayMes[$mes] : 'Não existe mês nesse número.';
+
+        $resultado = isset($arrayMes[$mes]) ? $arrayMes[$mes] : 'Não existe mês nesse número.';
+        return view('resultado', compact('resultado'));
     }
 
     public function questao3(Request $request)
     {
-        $notas = $request->post('nota');
-        $notaFinal = 0;
-        foreach ($notas as $nota){
-            $notaFinal += $nota;
-        }
-        
-        $media = ($notaFinal / count($notas)) >= 7.0;
-        return view('resultado', compact('media'));
+        $valores = $request->post('valores');
+        rsort($valores);        
+        return view('resultado', compact('valores'));
     }
 
     public function questao4(Request $request)
--- resources/views/resultado.blade.php
+++ resources/views/resultado.blade.php
@@ -5,3 +5,15 @@
     <p>Reprovado</p>
 @endif
 <hr>
+<h2>Resultado questao 2</h2>
+@if (isset($resultado))
+    <p>{{$resultado}}</p>
+@endif
+<hr>
+<h2>Resultado questao 3</h2>
+@if (isset($valores))
+    @foreach ($valores as $val)
+        <p>{{$val}}</p>
+    @endforeach
+@endif
+<hr>
